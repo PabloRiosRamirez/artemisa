c3ceef360cec2c5fbdec6be1f604774dafd4ae3f
John <jonathan.pereira.ing@gmail.com>
2019-07-28 00:19:06 -0400

    integracion uploadfile

--- src/main/java/online/grisk/artemisa/presentation/controller/DataIntegrationController.java
+++ src/main/java/online/grisk/artemisa/presentation/controller/DataIntegrationController.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -28,13 +31,15 @@ public class DataIntegrationController {
 	@Autowired
 	private DataIntegrationService dataIntegrationService;
 	
-//	@PostMapping("/DataIntegrations")
-//	private ResponseEntity<?> save(@RequestBody DataIntegration dataIntegration){
-//		DataIntegration di = dataIntegrationService.save(dataIntegration);
-//		if(di != null) {
-//			return new ResponseEntity<DataIntegration> 
-//		}
-//	}
+	@PostMapping("/dataIntegrations")
+	private ResponseEntity<?> save(@RequestBody DataIntegration dataIntegration){
+		DataIntegration di = dataIntegrationService.save(dataIntegration);
+		if(di != null) {
+			return new ResponseEntity<DataIntegration>(HttpStatus.CREATED);
+		}else {
+			return new ResponseEntity<String>("Error interno del servidor.", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
 	@PutMapping("/uploadFile/{id}")
 	public FileResponseDTO uploadFile(@PathVariable("id") long id, @RequestParam("file") MultipartFile file) {
