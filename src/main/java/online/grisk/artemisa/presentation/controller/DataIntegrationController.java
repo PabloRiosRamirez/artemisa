7da1e45bd490944890058b4b533484ec23e1670c
Pablo Rios Ramirez <pa.riosramirez@gmail.com>
2019-08-15 00:51:02 -0400

    Update DataIntegrationController.java

--- src/main/java/online/grisk/artemisa/presentation/controller/DataIntegrationController.java
+++ src/main/java/online/grisk/artemisa/presentation/controller/DataIntegrationController.java
@@ -19,7 +19,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
- 
+@RequestMapping({"/api/artemisa"})
 public class DataIntegrationController {
     private static final Logger logger = LoggerFactory.getLogger(DataIntegrationController.class);
 
